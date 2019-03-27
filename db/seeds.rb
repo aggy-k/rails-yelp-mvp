4b63c830cb970ff6ad758d9733f6419e5872b130
Agnes Selvita <a.selvita@gmail.com>
2019-03-27 14:24:16 +0800

    rails yelp review done with embedded review form in the restaurant show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up the restaurant db.."
+Restaurant.destroy_all if Rails.env.development?
+
+20.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: %w(chinese italian japanese french belgian).sample, phone_number: Faker::PhoneNumber.cell_phone, image: "https://picsum.photos/600/400/?random")
+end
+
+puts "Created #{Restaurant.count} new restaurant(s)"
